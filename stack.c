06fd542c4305289cfcec3a3576a7a3bd20fb4b4d
NedzunganiK <Nedzunganik8@gmail.com>
2023-12-17 07:53:53 -0800

    stacks queues solo project

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - does nothing, placeholder function for future implementation
+ * @head: pointer to the head of the stack
+ * @counter: line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
+
